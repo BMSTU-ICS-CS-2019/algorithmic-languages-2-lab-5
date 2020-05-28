262cee3c36ffb1f3a842d156bc7233ec988209a3
progrm_jarvis <gh@progrm-jarvis.ru>
2020-05-28 22:51:05 +0300

    Resolve allocator issues

--- include/vector.h
+++ include/vector.h
@@ -27,6 +27,7 @@ namespace collection {
         typedef Pointer Iterator;
         typedef ConstPointer ConstIterator;
         typedef Allocator AllocatorType;
+        typedef std::allocator_traits<AllocatorType> Memory;
 
     protected:
         static constexpr size_t DEFAULT_CAPACITY = 16;
@@ -39,7 +40,7 @@ namespace collection {
         /* ************************************************* Checks ************************************************* */
 
         inline void copyArrayNoChecks(ConstPointer originalArray, size_t const size) {
-            for (size_t i = 0; i < size; ++i) allocator_.construct(array_[i] = originalArray[i]);
+            for (size_t i = 0; i < size; ++i) Memory::construct(allocator_, array_ + i, *(originalArray + i));
         }
 
         inline void throwOutOfRange(size_t const index) {
@@ -59,22 +60,41 @@ namespace collection {
         /* ************************************************ Resizers ************************************************ */
 
         void resizeToBigger(size_t const newCapacity) {
-            auto* const newArray = allocator_.allocate(newCapacity);
-            std::move(array_, array_ + capacity_, newArray);
-            allocator_.deallocate(array_, capacity_);
+            // allocate new memory segment
+            Pointer const newArray = Memory::allocate(allocator_, newCapacity);
+            // move all currently constructed elements to the new memory location
+
+            { // move values
+                ConstPointer const sourceEnd = array_ + size_;
+                for (Pointer source = array_, target = newArray; source != sourceEnd; ++source) {
+                    Memory::construct(allocator_, target++, std::move(*source));
+                    Memory::destroy(allocator_, source);
+                }
+                Memory::deallocate(allocator_, array_, capacity_);
+            }
+
             array_ = newArray;
             capacity_ = newCapacity;
         }
 
         void resizeToSmaller(size_t const newCapacity) {
-            auto* const newArray = allocator_.allocate(newCapacity);
+            Pointer const newArray = Memory::allocate(allocator_, newCapacity);
             {
-                Pointer cutBegin = array_ + newCapacity;
-                std::copy(array_, cutBegin, newArray);
-                ConstPointer const cutEnd = array_ + capacity_;
-                for (; cutBegin != cutEnd; ++cutBegin) allocator_.destroy(cutBegin);
+                // move kept values
+                Pointer source;
+                {
+                    Pointer target;
+                    ConstPointer const moveSourceEnd = array_ + size_;
+                    for (source = array_, target = newArray; source != moveSourceEnd; ++source) {
+                        Memory::construct(allocator_, target++, std::move(source));
+                        Memory::destroy(allocator_, source);
+                    }
+                }
+                ConstPointer const sourceEnd = array_ + capacity_;
+                for (; source != sourceEnd; ++source) Memory::destroy(allocator_, source);
             }
-            allocator_.deallocate(array_, capacity_);
+            Memory::deallocate(allocator_, array_, capacity_);
+
             array_ = newArray;
             capacity_ = newCapacity;
         }
@@ -85,32 +105,121 @@ namespace collection {
 
         void requireExtraSlot() {
             auto const currentCapacity = capacity_;
-            if (size_ < currentCapacity) resizeToBigger(increasedCapacity(currentCapacity));
+            if (size_ == 0 || size_ == currentCapacity - 1) resizeToBigger(increasedCapacity(currentCapacity));
         }
 
         /* ****************************************** Internal constructor ****************************************** */
 
         // this does not perform object construction
-        Vector(size_t capacity, size_t const size)
-            : array_(allocator_.allocate(capacity)), size_(size), capacity_(capacity) {}
 
-        // this does not perform object construction
+        /**
+         * @brief Creates a vector of equal size and capacity without constructing its entries
+         * @param capacity capacity of the create vector, should not be less than {@code
+         * @param size size of the created vector
+         */
+        Vector(size_t capacity, size_t const size)
+            : array_(Memory::allocate(allocator_, capacity)), size_(size), capacity_(capacity) {}
+
+        /**
+         * @brief Creates a vector of equal size and capacity without constructing its entries
+         * @param size size of the created vector and its capacity
+         */
         Vector(size_t const size) : Vector(size, size) {}
+
+        /* ************************************* Internal complicated modifiers ************************************* */
+
+
+        void uncheckedInsert(Iterator position, ConstReference value) {
+            // this should happen before reallocation as after the pointers change
+            auto const index = position - array_;
+            requireExtraSlot();
+
+            auto const oldSize = size_++;
+            if (index == oldSize) Memory::construct(allocator_, array_ + size_, value);
+            else {
+                // the element should be constructed anyway, but using previous value
+                auto target = array_ + oldSize;
+                Memory::construct(allocator_, target, std::move(*(target - 1)));
+                // the elements after index should be moved to the right
+                // target is the element to move the value into
+                auto const bound = array_ + index;
+                for (; target != bound;) {
+                    auto const current = target--; // actual target to move the value into
+                    *current = std::move(*target);
+                }
+                // here target is the bound and contains the value to be destroyed
+                Memory::destroy(allocator_, target);
+                Memory::construct(allocator_, target, value);
+            }
+        }
+
+        void uncheckedInsert(ConstIterator const position, RValueReference value) {
+            // this should happen before reallocation as after the pointers change
+            auto const index = position - array_;
+            requireExtraSlot();
+
+            auto const oldSize = size_++;
+            if (index == oldSize) Memory::construct(allocator_, array_ + oldSize, std::forward<RValueReference>(value));
+            else {
+                // the element should be constructed anyway, but using previous value
+                auto target = array_ + oldSize;
+                Memory::construct(allocator_, target, std::move(*(target - 1)));
+                // the elements after index should be moved to the right
+                // target is the element to move the value into
+                auto const bound = array_ + index;
+                for (; target != bound;) {
+                    auto const current = target--; // actual target to move the value into
+                    *current = std::move(*target);
+                }
+                // here target is the bound and contains the value to be destroyed,
+                // so we perform the last exchange - with the caller
+                *target = std::forward<RValueReference>(value);
+            }
+        }
+
+        void uncheckedErase(ConstIterator const from, ConstIterator const to) noexcept {
+            auto const end = cend();
+            // move all elements after (to) to the left
+            // target is the element on the left to which the element gets moved
+            // source is the element on the right from which the element gets moved
+            for (auto source = to, target = from; source != end; ++source) *target = std::move(*source);
+            auto const delta = to - from;
+            for (auto iterator = end - delta; iterator != end; ++iterator) Memory::destroy(allocator_, iterator);
+
+            size_ -= delta;
+        }
 
     public:
         /* *********************************************** Destructor *********************************************** */
 
-        ~Vector() {
-            for (size_t i = 0; i < size_; ++i) allocator_.destroy(array_ + i);
-            allocator_.deallocate(array_, capacity_);
+        /**
+         * @brief Destroys this vector freeing all allocated memory
+         *
+         * @note while marking this as {@code virtual} may bring possible overhead, this allows safe extension
+         * of this class, while most (if not all) modern compilers are able to perform monomorphic inline caching
+         */
+        virtual ~Vector() noexcept {
+            for (size_t i = 0; i < size_; ++i) Memory::destroy(allocator_, array_ + i);
+            Memory::deallocate(allocator_, array_, capacity_);
         }
 
         /* ********************************************** Constructors ********************************************** */
 
+        /**
+         * @brief Creates a new empty vector of default capacity.
+         */
         Vector() : Vector(DEFAULT_CAPACITY, 0) {}
 
+        /**
+         * @brief Copy-constructs a vector from the specified one.
+         * @param original vector whose contents should be {@bold copied} into this one
+         */
         Vector(Vector const& original) : Vector(original.size_) { copyArrayNoChecks(original.array_); }
 
+        /**
+         * @brief Move-constructs a vector from the specified one.
+         * @param original vector whose contents should be {@bold moved} into this one
+         */
         Vector(Vector&& original) noexcept
             : array_(std::exchange(original.array_, nullptr)), size_(std::exchange(original.size_, 0)),
               capacity_(std::exchange(original.capacity_, 0)) {}
@@ -120,7 +229,7 @@ namespace collection {
         Vector& operator=(Vector const& other) {
             if (this != *other) {
                 if (size_ < other.size_) {
-                    array_ = allocator_.allocate(other.size_);
+                    array_ = Memory::allocate(allocator_, other.size_);
                     size_ = other.size_;
                     capacity_ = other.capacity_;
                 }
@@ -146,9 +255,9 @@ namespace collection {
 
         /* ********************************************* Indexed access ********************************************* */
 
-        reference operator[](size_t index) { return array_[index]; }
+        reference operator[](size_t const index) { return array_[index]; }
 
-        ConstReference operator[](size_t index) const { return array_[index]; }
+        ConstReference operator[](size_t const index) const { return array_[index]; }
 
         reference at(size_t const index) {
             checkRange(index);
@@ -198,86 +307,85 @@ namespace collection {
             if (capacity_ < newCapacity) resizeToBigger(newCapacity);
         }
 
-        void resize(size_t const newSize, ValueType value /* = {}*/) {
-            auto const currentSize = size_;
-            if (newSize < currentSize) size_ = newSize;
-            else if (newSize > currentSize) {
-                resizeToBigger(newSize);
-                std::fill(array_ + currentSize, array_ + newSize, value);
-                size_ = newSize;
-            }
-        }
-
         void resize(size_t const newSize) {
+            static_assert(std::is_default_constructible<ValueType>::value, "Type should be default-constructable");
+
             auto const currentSize = size_;
-            if (newSize < currentSize) size_ = newSize;
-            else if (newSize > currentSize) {
-                resizeToBigger(newSize);
+            if (newSize < currentSize) {
+                for (size_t i = newSize; i < currentSize; ++i) Memory::destroy(allocator_, array_ + i);
+                size_ = newSize; // simply decrease size
+            } else if (newSize > currentSize) {
+                if (newSize > capacity_) resizeToBigger(newSize);
+                for (size_t i = currentSize; i < newSize; ++i) Memory::construct(allocator_, array_ + i, ValueType{});
+                // allocate new, bigger array
                 size_ = newSize;
             }
         }
 
         void resize(size_t const newSize, ConstReference value) {
             auto const currentSize = size_;
-            if (newSize < currentSize) size_ = newSize;
-            else if (newSize > currentSize) {
-                resizeToBigger(newSize);
-                std::fill(array_ + currentSize, array_ + newSize, value);
+            if (newSize < currentSize) {
+                for (size_t i = newSize; i < currentSize; ++i) Memory::destroy(allocator_, array_ + i);
+                size_ = newSize; // simply decrease size
+            } else if (newSize > currentSize) {
+                if (newSize > capacity_) resizeToBigger(newSize);
+                for (size_t i = currentSize; i < newSize; ++i) Memory::construct(allocator_, array_ + i, value);
+                // allocate new, bigger array
                 size_ = newSize;
             }
         }
 
         // differs from standard implementation as it
         void clear() {
-            for (size_t i = 0; i < size_; ++i) allocator_.destroy(array_ + i);
+            for (size_t i = 0; i < size_; ++i) Memory::destroy(allocator_, array_ + i);
             size_ = 0;
         }
 
         void insert(Iterator position, ConstReference value) {
-            requireExtraSlot();
-            // move all elements to the right
-            for (auto iterator = array_ + ++size_; iterator != position;) *iterator = *(iterator - 1);
-            *position = value;
+            if (position < array_) throw std::range_error("`position` is out of lower range");
+            if (position > array_ + 1) throw std::range_error("`position` is out of higher range");
+
+            uncheckedInsert(position, value);
         }
 
-        void insert(Iterator position, RValueReference value) {
-            auto const offset = position - array_;
-            requireExtraSlot();
-            position = array_ + offset;
-            // move all elements to the right
-            for (auto iterator = array_ + ++size_; iterator != position; --iterator) *iterator = *(iterator - 1);
-            *position = std::move(value);
+        void insert(ConstIterator const position, RValueReference value) {
+            if (position < array_) throw std::range_error("`position` is out of lower bound");
+            if (position > cend()) throw std::range_error("`position` is out of higher bound");
+
+            uncheckedInsert(position, std::forward<RValueReference>(value));
         }
 
-        void erase(Iterator from, Iterator to) { // spec does not require any explicit exceptions
-            auto const delta = to - from;
-            auto const arrayEnd = end();
-            // move all elements after (to) to the right
-            for (auto target = from, source = to; source != arrayEnd;) *(target++) = *(source++);
-            // destroy tail
-            for (auto iterator = arrayEnd - delta; iterator != arrayEnd;) allocator_.destroy(iterator++);
+        void erase(ConstIterator const from, ConstIterator const to) {
+            if (from > to) throw std::logic_error("`from` cannot be after `to`");
+            if (from < array_) throw std::range_error("`from` is out of lower bound");
+            if (to < array_) throw std::range_error("`to` is out of lower bound");
+            auto const end = cend();
+            if (from > end) throw std::range_error("`from` is out of higher bound");
+            if (to > end) throw std::range_error("`to` is out of higher bound");
 
-            size_ -= delta;
+            uncheckedErase(from, to);
         }
 
-        void erase(Iterator position) { erase(position, position + 1); }
+        void erase(Iterator const position) { erase(position, position + 1); }
 
         void pushBack(ConstReference value) {
             requireExtraSlot();
 
-            array_[size_++] = value;
+            auto* const address = array_ + size_++;
+            Memory::construct(allocator_, address, value);
         }
 
         void pushBack(RValueReference value) {
             requireExtraSlot();
 
-            array_[size_++] = std::move(value);
+            auto* const address = array_ + size_++;
+            Memory::construct(allocator_, address, std::forward<RValueReference>(value));
         }
 
         void popBack() {
             checkNotEmpty();
 
-            resizeToSmaller(--size_);
+            Memory::destroy(allocator_, array_ + (--size_));
         }
     };
 } // namespace collection
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <ostream>
 #include <vector.h>
 
 #define _VECTOR_SHOWCASE(variable, operation)                                                                          \
@@ -10,30 +11,80 @@
     for (auto const element : vector) std::cout << element << '\t';                                                    \
     std::cout << '}' << std::endl;
 
+class CustomStruct {
+    std::string text;
+    int number;
+
+public:
+    CustomStruct(std::string text, int number) : text(text), number(number) {}
+
+    CustomStruct() : CustomStruct("default text", 0xCAFEBABE) {}
+
+    friend std::ostream& operator<<(std::ostream& output, CustomStruct const& customStruct);
+};
+
+std::ostream& operator<<(std::ostream& output, CustomStruct const& customStruct) {
+    std::cout << "CustomStruct{name=\"" << customStruct.text << "\", number=" << customStruct.number << "}";
+
+    return output;
+}
+
 int main() {
-    collection::Vector<int> vector;
-    _VECTOR_SHOWCASE(vector, pushBack(1))
-    _VECTOR_SHOWCASE(vector, pushBack(5))
-    _VECTOR_SHOWCASE(vector, pushBack(127))
-    _VECTOR_SHOWCASE(vector, popBack())
-    _VECTOR_SHOWCASE(vector, popBack())
-    _VECTOR_SHOWCASE(vector, pushBack(2))
-    _VECTOR_SHOWCASE(vector, pushBack(4))
-    _VECTOR_SHOWCASE(vector, pushBack(8))
-    _VECTOR_SHOWCASE(vector, insert(vector.begin() + 2, 99))
-    _VECTOR_SHOWCASE(vector, erase(vector.begin() + 1))
-    _VECTOR_SHOWCASE(vector, erase(vector.begin() + 1, vector.begin() + 3))
-    _VECTOR_SHOWCASE(vector, clear())
-    _VECTOR_SHOWCASE(vector, pushBack(127))
-    _VECTOR_SHOWCASE(vector, pushBack(6))
-    _VECTOR_SHOWCASE(vector, pushBack(8))
-    _VECTOR_SHOWCASE(vector, pushBack(1))
-    _VECTOR_SHOWCASE(vector, resize(8))
-    _VECTOR_SHOWCASE(vector, reserve(16))
-    _VECTOR_SHOWCASE(vector, resize(2))
-    _VECTOR_SHOWCASE(vector, pushBack(1))
-    _VECTOR_SHOWCASE(vector, pushBack(2))
-    _VECTOR_SHOWCASE(vector, pushBack(3))
-    _VECTOR_SHOWCASE(vector, pushBack(4))
-    _VECTOR_SHOWCASE(vector, reserve(3))
+    std::cout << "========== Vector<int> ==========" << std::endl;
+    {
+        collection::Vector<int> vector;
+        _VECTOR_SHOWCASE(vector, pushBack(1))
+        _VECTOR_SHOWCASE(vector, pushBack(5))
+        _VECTOR_SHOWCASE(vector, pushBack(127))
+        _VECTOR_SHOWCASE(vector, popBack())
+        _VECTOR_SHOWCASE(vector, popBack())
+        _VECTOR_SHOWCASE(vector, pushBack(2))
+        _VECTOR_SHOWCASE(vector, pushBack(4))
+        _VECTOR_SHOWCASE(vector, pushBack(8))
+        _VECTOR_SHOWCASE(vector, insert(vector.begin() + 2, 99))
+        _VECTOR_SHOWCASE(vector, erase(vector.begin() + 1))
+        _VECTOR_SHOWCASE(vector, erase(vector.begin() + 1, vector.begin() + 3))
+        _VECTOR_SHOWCASE(vector, clear())
+        _VECTOR_SHOWCASE(vector, pushBack(127))
+        _VECTOR_SHOWCASE(vector, pushBack(6))
+        _VECTOR_SHOWCASE(vector, pushBack(8))
+        _VECTOR_SHOWCASE(vector, pushBack(1))
+        _VECTOR_SHOWCASE(vector, resize(8))
+        _VECTOR_SHOWCASE(vector, reserve(16))
+        _VECTOR_SHOWCASE(vector, resize(2))
+        _VECTOR_SHOWCASE(vector, pushBack(1))
+        _VECTOR_SHOWCASE(vector, pushBack(2))
+        _VECTOR_SHOWCASE(vector, pushBack(3))
+        _VECTOR_SHOWCASE(vector, pushBack(4))
+        _VECTOR_SHOWCASE(vector, reserve(3))
+    }
+
+    std::cout << "========== Vector<CustomStruct> ==========" << std::endl;
+    {
+        collection::Vector<CustomStruct> vector;
+        _VECTOR_SHOWCASE(vector, pushBack({"one", 1}))
+        _VECTOR_SHOWCASE(vector, pushBack({"five", 5}))
+        _VECTOR_SHOWCASE(vector, pushBack({"one hundred and twenty seven", 127}))
+        _VECTOR_SHOWCASE(vector, popBack())
+        _VECTOR_SHOWCASE(vector, popBack())
+        _VECTOR_SHOWCASE(vector, pushBack({"two", 2}))
+        _VECTOR_SHOWCASE(vector, pushBack({"four", 4}))
+        _VECTOR_SHOWCASE(vector, pushBack({"eight", 8}))
+        _VECTOR_SHOWCASE(vector, insert(vector.begin() + 2, {"ninety nine", 99}))
+        _VECTOR_SHOWCASE(vector, erase(vector.begin() + 1))
+        _VECTOR_SHOWCASE(vector, erase(vector.begin() + 1, vector.begin() + 3))
+        _VECTOR_SHOWCASE(vector, clear())
+        _VECTOR_SHOWCASE(vector, pushBack({"one hundred and twenty seven", 127}))
+        _VECTOR_SHOWCASE(vector, pushBack({"six", 6}))
+        _VECTOR_SHOWCASE(vector, pushBack({"eight", 8}))
+        _VECTOR_SHOWCASE(vector, pushBack({"one", 1}))
+        _VECTOR_SHOWCASE(vector, resize(8))
+        _VECTOR_SHOWCASE(vector, reserve(16))
+        _VECTOR_SHOWCASE(vector, resize(2))
+        _VECTOR_SHOWCASE(vector, pushBack({"one", 1}))
+        _VECTOR_SHOWCASE(vector, pushBack({"two", 2}))
+        _VECTOR_SHOWCASE(vector, pushBack({"three", 3}))
+        _VECTOR_SHOWCASE(vector, pushBack({"four", 4}))
+        _VECTOR_SHOWCASE(vector, reserve(3))
+    }
 }
